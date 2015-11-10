d1332533dbe711be74e3bcc3377126fd59e33635
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2015-11-10 21:11:17 +0100

    finished Aufgabe2

--- src/university/Aufgabe2/tasks/Aufgabe2.java
+++ src/university/Aufgabe2/tasks/Aufgabe2.java
@@ -19,11 +19,42 @@
 
     Zusatzfragen:
     1. Welche Unterschiede konnten Sie zwischen der rekursiven und iterativen Implementierung feststellen?
+       TODO: habe keine iterative Version programmiert
 */
 public class Aufgabe2 {
 
     // just for testing ...
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        drawNumDiamond(9);
     }
+
+    public static void drawNumDiamond(int h) throws Exception {
+        if (h < 0) {
+            throw new Exception("height must be a positive number");
+        }
+        drawNumDiamondHelper((h % 2) == 1 ? h + 1 : h, 1);
+    }
+
+    private static void drawNumDiamondHelper(int height, int position) {
+        if (height <= position) {
+            return;
+        }
+        int letterIndex = (height - position) < position ? (height - position): position;
+        int letterCount = letterIndex + (letterIndex - 1);
+        String line = repeat(" ", (height - letterCount) / 2) + repeat("" + letterIndex, letterCount) + repeat(" ", (height - letterCount) / 2);
+        System.out.println(line);
+        drawNumDiamondHelper(height, position + 1);
+    }
+
+    private static String repeat(String s, int n) {
+        if(s == null) {
+            return null;
+        }
+        final StringBuilder sb = new StringBuilder(s.length() * n);
+        for(int i = 0; i < n; i++) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+
 }
