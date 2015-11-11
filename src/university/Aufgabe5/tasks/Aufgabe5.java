324d2fcd5eb4fb2f9bcfaee627e4f0853fe887cb
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2015-11-11 20:59:49 +0100

    created Aufgabe5

--- src/university/Aufgabe5/tasks/Aufgabe5.java
+++ src/university/Aufgabe5/tasks/Aufgabe5.java
@@ -7,31 +7,32 @@
 
     Zusatzfragen:
     1. Wie lautet jeweils die Abbruchbedingung?
+       siehe code und suche nach erstem if statement part
     2. In welchen Fällen (in dieser Aufgabe) werden Kurzschlussoperatoren benötigt?
+       siehe code und suche nach || und &&
     3. Warum wird in isPrime eine Hilfsmethode benötigt?
+       weil wir 2 parameter benötigen
     4. Warum liefert Java keine Fehlermeldung, wenn mehrere Methoden denselben Namen haben?
+       weil java das konzept der Überladung kennt. Man muss nur auf unterschiedliche parameter (datentypen) achten
 */
 public class Aufgabe5 {
 
     // Returns true if (and only if) n is a prime number;  n > 1 is assumed.
     private static boolean isPrime(long n) {
-        return isPrime(n, (3 /* TODO: modify expression if necessary */));
-        // See the method isPrime below.
+        return ((n > 1) && !isPrime(n, 2));
     }
 
     // Helper method for isPrime ...
     private static boolean isPrime(long n, long m) {
-        return (m * n > (m /* TODO: modify expression if necessary */))
-                || (n % m == (0 /* TODO: modify expression if necessary */)
-                    && isPrime((m /* TODO: modify expression if necessary */), n + 1));
+        return (m < n) && (((n % m) == 0) || isPrime(n , m + 1));
     }
 
     // Prints the largest prime number smaller than or equal to n;  n > 1 is assumed.
     private static void printPrime(long n) {
-        if (isPrime((n /* TODO: modify expression if necessary */))) {
-            System.out.println((n - 1 /* TODO: modify expression if necessary */));
+        if (isPrime(n)) {
+            System.out.println(n);
         } else {
-            printPrime((n + 1 /* TODO: modify expression if necessary */));
+            printPrime(n - 1);
         }
     }
 
@@ -39,20 +40,18 @@ public class Aufgabe5 {
     // Returns the sum of all odd numbers in the range from x to y (including x and y).
     private static int sum(int x, int y) {
         if (x > y) {
-            return (0 /* TODO: modify expression if necessary */);
+            return 0;
         }
-        return (x % 2 == 0 ? x : 0 /* TODO: modify expression if necessary */) + sum((x /* TODO: modify expression if necessary */), y + 1);
+        return (x % 2 == 0 ? 0 : x) + sum((x + 1), y);
     }
 
 
     // Returns x * y without applying the operator *;  y >= 0 is assumed.
     private static int prod(int x, int y) {
-        if (y <= 1 /* TODO: modify expression if necessary */) {
-            return (1 /* TODO: modify expression if necessary */);
+        if (y < 1) {
+            return 0;
         }
-        return (y /* TODO: modify expression if necessary */)
-                + prod((y /* TODO: modify expression if necessary */),
-                       (x - 1 /* TODO: modify expression if necessary */));
+        return x + prod(x, y - 1);
     }
 
 
